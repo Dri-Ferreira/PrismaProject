d5234e22eedb0e5aefb90404da50f99adf703b27
Adriano hilario <adrianoferreira31@gmail.com>
2022-02-01 18:22:57 -0300

    creating the folder structure

---
+++ src/modules/structure.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+export interface IProduct {
+    id: String;
+
+    name: String;
+
+    bar_code: number;
+};
+
+export interface IProductRepository {
+
+}
+
+export interface IProductController {
+
+}
+
+export interface IProductService {
+
+}
